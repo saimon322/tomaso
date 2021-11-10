50ba22fead4e728b36170dabc584eccf79060d7a
_Skyline_ <kirilin.1997@mail.ru>
2021-11-10 18:35:37 +0300

    Create project

--- assets/src/js/common.js
+++ assets/src/js/common.js
@@ -1,4 +1,8 @@
 // DOM content load
 $(() => {
-    
+    document.querySelector('.hamburger').addEventListener('click', () => {
+        console.log(111111);
+        document.getElementsByClassName('hamburger').classList.toggle("main--main-bg");
+        this.classList.add('close');
+    });
 })
