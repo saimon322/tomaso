bca2f5010d573da29d2dd8053455fa9baded42ba
_Skyline_ <kirilin.1997@mail.ru>
2021-11-12 16:05:52 +0300

    added main slider

--- assets/src/js/sliders.js
+++ assets/src/js/sliders.js
@@ -1,26 +1,43 @@
 import Swiper from 'swiper/swiper-bundle.min.js';
 
 
-const startupsSlider = document.querySelector('.slider__main-vertical');
+const startupsSlider = document.querySelector('.slider--main-vertical .swiper-slider');
 
 const bp576 = window.matchMedia('(min-width: 576px)');
 let step = 0;
 
 if (startupsSlider) {
     const startups = new Swiper(startupsSlider, {
-        loop: true,
-        // slidesOffsetAfter: 40,
-        slidesOffsetBefore: 40,
-        spaceBetween: 10,
-        slidesPerView: 1.25,
+        // loop: true,
         speed: 500,
+        direction: 'vertical',
         autoHeight: true,
+
         pagination: {
             el: '.main-vertical--pagination',
             clickable: true,
             renderBullet: function (index, className) {
-                return '<span class="' + className + '" data-year="' + this.slides[index].dataset.year + '"></span>';
+                return '<span class="' + className + '"></span>';
             },
+        },
+        navigation: {
+            nextEl: '.main-vertical__next',
+            prevEl: '.main-vertical__prev',
+        },
+        breakpoints: {
+            0: {
+                direction: 'horizontal',
+                spaceBetween: 10,
+                slidesPerView: 'auto',
+                centeredSlides: true,
+                autoHeight: true,
+                navigation: false,
+            },
+            768: {
+                direction: 'vertical',
+                slidesPerView: 1,
+                autoHeight: false,
+            }
         },
     });
 
