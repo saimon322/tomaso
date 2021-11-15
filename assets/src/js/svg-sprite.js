57f10e5af2a578644856407fb106c7fb65c3567f
Morfeuschannel <morfeuschannel@gmail.com>
2021-11-15 04:45:07 +0300

    green banner update and about block

--- assets/src/js/svg-sprite.js
+++ assets/src/js/svg-sprite.js
@@ -3,6 +3,7 @@ import '../img/svg-sprite/logo_text.svg';
 import '../img/svg-sprite/hamburger.svg';
 import '../img/svg-sprite/arrow-down.svg';
 import '../img/svg-sprite/close.svg';
+import '../img/svg-sprite/circle_arrow--down.svg';
 
 // slider
 import '../img/svg-sprite/slider_arrow--left.svg';
