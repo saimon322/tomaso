f0b236103cbf61c97ddeb137a697705131bebf43
_Skyline_ <kirilin.1997@mail.ru>
2021-11-17 10:02:40 +0300

    update backend

---
+++ page-about_meat.php
@@ -0,0 +1,29 @@
+<?php
+/*
+* Template Name: О мясе
+*/
+get_header(); ?>
+<main class="main">
+
+    <?php
+    get_template_part('template-parts/main-banner');
+
+    get_template_part('template-parts/about');
+
+    get_template_part('template-parts/section-green');
+
+    get_template_part('template-parts/about-page/about-slider');
+
+    get_template_part('template-parts/products-slider');
+
+    get_template_part('template-parts/about-page/section-green-second');
+
+    get_template_part('template-parts/where-buy');
+
+    get_template_part('template-parts/about-page/culture');
+
+    get_template_part('template-parts/bottom-banner');
+    ?>
+
+</main>
+<?php get_footer(); ?>
