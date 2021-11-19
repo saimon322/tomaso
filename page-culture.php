e7c697ee920fcc6fa11ff1ae179c08e220c82425
_Skyline_ <kirilin.1997@mail.ru>
2021-11-19 10:36:36 +0300

    back: added culture page

---
+++ page-culture.php
@@ -0,0 +1,23 @@
+<?php
+/*
+* Template Name: Культура потребления
+*/
+get_header(); ?>
+<main class="main">
+
+    <?php
+    get_template_part('template-parts/main-banner');
+
+    get_template_part('template-parts/about');
+
+    get_template_part('template-parts/section-green');
+
+    get_template_part('template-parts/culture/attitude');
+
+    get_template_part('template-parts/about', '', array('n' => '_2'));
+
+    get_template_part('template-parts/bottom-banner');
+    ?>
+
+</main>
+<?php get_footer(); ?>
