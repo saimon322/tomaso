c03e0b17c580df0385452dcadd31ac012342f609
Morfeuschannel <morfeuschannel@gmail.com>
2021-11-30 19:52:27 +0300

    bug fixes

--- assets/src/js/sliders.js
+++ assets/src/js/sliders.js
@@ -107,7 +107,7 @@ if (aboutSlider) {
 
         breakpoints: {
             1024: {
-                centeredSlides: true,
+                slidesPerView: 1,
                 pagination: false,
                 autoplay: {
                     enabled: true,
@@ -125,7 +125,6 @@ if (antibioSlider) {
         loop: true,
         speed: 500,
         slidesPerView: 1,
-        autoHeight: true,
 
         pagination: {
             el: '.antibio-slider__pagination',
